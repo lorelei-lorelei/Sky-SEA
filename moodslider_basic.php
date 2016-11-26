95547bb3b479dfabc16fdae3c73de85dbf7b4745
Kim Lofthouse <kimmiikal@gmail.com>
2016-11-26 14:52:02 +0000

    Format changes

--- moodslider_basic.php
+++ moodslider_basic.php
@@ -7,73 +7,79 @@
 </head>
 <body>
   <div class="header">
+    <img src="images/sky_cinema_logo.png" />
     <h1>Moodslider - Sky Cinema</h1>
-    <img src="images/sky_cinema_logo.png" align=left />
   </div>
 
-  <div class="body"><textarea rows="2" cols="50">
+  <div class="upload">
+    <textarea rows="2" cols="50">
     Moodslider|Upload
-  </textarea>
+    </textarea>
+ </div>
+
+  <br>
+  <div class="slider">
+    <span id="range">Agitated</span>
+    <input type="range" min="0" max="50" value="0"step="5" onchange="showValue(this.value)" />
+    <span id="range">Calm</span>
+      <script type="text/javascript">
+        function showValue(newValue)
+         {
+	        document.getElementById("range").innerHTML=newValue;
+         }
+      </script>
 
   <br>
 
-  <span id="range">Agitated</span>
-  <input type="range" min="0" max="50" value="0"step="5" onchange="showValue(this.value)" />
-  <span id="range">Calm</span>
-  <script type="text/javascript">
-    function showValue(newValue)
-    {
-	    document.getElementById("range").innerHTML=newValue;
-    }
-  </script>
-
-  <br>
-
-  <span id="range">Happy</span>
-  <input type="range" min="0" max="50" value="0"step="5" onchange="showValue(this.value)" />
-  <span id="range">Sad</span>
-  <script type="text/javascript">
-    function showValue(newValue)
-    {
-      document.getElementById("range").innerHTML=newValue;
-    }
-   </script>
+    <span id="range">Happy</span>
+     <input type="range" min="0" max="50" value="0"step="5" onchange="showValue(this.value)" />
+    <span id="range">Sad</span>
+      <script type="text/javascript">
+        function showValue(newValue)
+       {
+         document.getElementById("range").innerHTML=newValue;
+       }
+     </script>
 
   <br>
 
   <span id="range">Tired</span>
-  <input type="range" min="0" max="50" value="0"step="5" onchange="showValue(this.value)" />
+    <input type="range" min="0" max="50" value="0"step="5" onchange="showValue(this.value)" />
   <span id="range">Wide Awake</span>
-  <script type="text/javascript">
-    function showValue(newValue)
-    {
-      document.getElementById("range").innerHTML=newValue;
-    }
-   </script>
+    <script type="text/javascript">
+      function showValue(newValue)
+      {
+        document.getElementById("range").innerHTML=newValue;
+      }
+    </script>
 
   <br>
 
   <span id="range">Scared</span>
-  <input type="range" min="0" max="50" value="0"step="5" onchange="showValue(this.value)" />
+    <input type="range" min="0" max="50" value="0"step="5" onchange="showValue(this.value)" />
   <span id="range">Fearless</span>
-  <script type="text/javascript">
-    function showValue(newValue)
-    {
-      document.getElementById("range").innerHTML=newValue;
-    }
-   </script>
+    <script type="text/javascript">
+      function showValue(newValue)
+      {
+        document.getElementById("range").innerHTML=newValue;
+      }
+    </script>
+ </div>
 
    <br>
+  <div class="result">
+     <form action="">
+      <input type="text" name="nocontent" placeholder="no content"><br>
+      <input type="submit" value="no content"><br>
+      <input type="text" name="nocontent" placeholder="no content"><br>
+      <input type="submit" value="no content"><br>
+      <input type="text" name="nocontent" placeholder="no content"><br>
+      <input type="submit" value="no content"><br>
+      <input type="text" name="nocontent" placeholder="no content"><br>
+      <input type="submit" value="no content"><br>
+      <input type="text" name="nocontent" placeholder="no content"><br>
+      <input type="submit" value="no content"><br>
+    </form>
+  </div>
 
-   <form action="">
-    <input type="text" name="nocontent" placeholder="no content"><br>
-    <input type="submit" value="no content"><br>
-    <input type="text" name="nocontent" placeholder="no content"><br>
-    <input type="submit" value="no content"><br>
-    <input type="text" name="nocontent" placeholder="no content"><br>
-    <input type="submit" value="no content"><br>
-    <input type="text" name="nocontent" placeholder="no content"><br>
-    <input type="submit" value="no content"><br>
-  </form>
-</div>
 </body>
