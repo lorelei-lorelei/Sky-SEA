2220a67c4463769e6f98b50304252daebd5240e2
Kim Lofthouse <kimmiikal@gmail.com>
2016-11-26 18:07:29 +0000

    Updated code - incomplete

---
+++ moodchoice.php
@@ -0,0 +1,38 @@
+<?php
+
+include 'moodslider_basic.php';
+
+$agicalm = ($_GET["agitated_calm"]);
+
+if ($agicalm == 50){
+  display("");
+} elseif($agicalm == 0){
+  display("");
+}
+
+$happysad = ($_GET["happy_sad"]);
+
+if ($happysad == 50){
+  display();
+} elseif($happysad == 0){
+  display("");
+}
+
+$tiredawake = ($_GET["tired_awake"]);
+
+if ($tiredawake == 50){
+  display("");
+} elseif($tiredawake == 0){
+  display("");
+}
+
+
+$scared = ($_GET["scared"]);
+
+if ($scared == 50){
+  display("");
+} elseif($scared == 0){
+  display("");
+}
+
+?>
