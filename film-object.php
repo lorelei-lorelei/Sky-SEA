dc4dafcdea92447916e4624699a853eea262b18f
Kim Lofthouse <kimmiikal@gmail.com>
2016-11-27 11:59:50 +0000

    Create film object

---
+++ film-object.php
@@ -0,0 +1,25 @@
+<?php
+$filmsArray = [];
+$xml = simplexml_load_file( 'films.xml');
+  foreach ($xml as $film) {
+    $film = new Film($film->name, $film->mood, $film->id, $film->image);
+    echo ($film->title);
+    array_push($filmsArray, $film);
+  }
+class Film {
+  var $title;
+  var $mood;
+  var $id;
+  var $image;
+  function __construct($new_title, $new_mood, $new_id, $new_image) {
+			$this->title = $new_title;
+      $this->mood = $new_mood;
+      $this->id = $new_id;
+      $this->image = $new_image;
+    return $this->title;
+    return $this->mood;
+    return $this->id;
+    return $this->image;
+  }
+}
+ ?>
