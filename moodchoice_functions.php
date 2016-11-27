623472dfa6af5fbe93d6572c549d643b06aa39f4
Kim Lofthouse <kimmiikal@gmail.com>
2016-11-27 14:26:40 +0000

    Create new function

--- moodchoice_functions.php
+++ moodchoice_functions.php
@@ -1,15 +1,14 @@
 <?php
 
-include 'moodslider.php';
 include 'film-object.php';
 
-$agicalm = ($_GET["agitated_calm"]);
-$happysad = ($_GET["happy_sad"]);
-$tiredawake = ($_GET["tired_awake"]);
-$scared = ($_GET["scared"]);
-
 function film_suggest(){
-  if ($agicalm == 0){
-    echo $agitatedfilm->id = '3';
+$mood = ($_GET["ag_calm"]);
+  if ($mood == 0){
+    echo $mood = "Agitated";
+  } else {
+    echo $film;
   }
 }
+
+film_suggest();
