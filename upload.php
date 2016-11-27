92c285f1e3d4ace3265000a516bc7f36fd942c32
Kim Lofthouse <kimmiikal@gmail.com>
2016-11-27 15:41:27 +0000

    Create upload page

---
+++ upload.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+
+<form action="upload.php" method="post">
+  <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+  <label for "file_upload">Upload a file:</label>
+    <input name="file_upload" type="file">
+<button type="submit">Upload</button>
+</form>
+
+<?php
+$filename = ($_FILES[""]);
+echo $filename;
+
+?>
